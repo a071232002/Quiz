c2aa167ea569c8c1adee1128cf6e70b981b06637
Eric Wen <erichjw@A04M2120118.mitac.com.tw>
2024-11-04 14:19:52 +0800

    T0100, T104

---
+++ src/main/java/com/quiz/leetcode/T0104MaximumDepthOfBinaryTree.java
@@ -0,0 +1,30 @@
+package com.quiz.leetcode;
+
+import org.w3c.dom.Node;
+
+import javax.swing.tree.TreeNode;
+
+public class T0104MaximumDepthOfBinaryTree {
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return left > right ? left + 1 : right + 1;
+    }
+
+}
